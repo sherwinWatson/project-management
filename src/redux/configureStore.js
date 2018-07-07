5fefee4d3bf63d984a77200b522a9d5b1864c943
sherwin <sherwin.watson@ymail.com>
2018-07-07 19:32:06 +0700

    add persister on users

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,18 @@ import { Text, Platform, NativeModules } from 'react-native'
 import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { combineReducers } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import reducers from './reducers'
 import sagas from './sagas'
 import IS_DEBUG from './../../App'
 
 export const IS_WEB = false
+
+const persistConfig = {
+  key: 'users',
+  storage,
+}
 
 export default (Router, onComplete) => {
   let composeEnhancers
@@ -42,12 +49,14 @@ export default (Router, onComplete) => {
     ...mobileReducers,
   })
 
+  const persistedReducer = persistReducer(persistConfig, appReducers)
   const sagasMiddleware = createSagaMiddleware()
   const middlewares = applyMiddleware(sagasMiddleware)
   const enhancers = composeEnhancers(middlewares)
-  const store = createStore(appReducers, enhancers)
+  const store = createStore(persistedReducer, enhancers)
+  const persistor = persistStore(store)
 
   sagasMiddleware.run(sagas)
 
-  return store
+  return { store, persistor }
 }
