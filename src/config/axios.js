f39e5353c276135b8086836fce9e82f4d1b656b5
Alpin <alpin_taise@yahoo.com>
2018-08-23 23:08:38 +0700

    fix axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export const axiosInit = (token) => {
-  // axios.defaults.baseURL = 'http://35.197.143.46/api/'
-  axios.defaults.baseURL = 'http://192.168.43.242:8000/api/'
+  axios.defaults.baseURL = 'http://35.197.143.46/api/'
   axios.defaults.timeout = 15000
 
   if (token) {
