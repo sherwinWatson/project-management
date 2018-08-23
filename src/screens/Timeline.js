7497436935f3b4314f5687b5321d00eed14464f6
sherwin <sherwin.watson@ymail.com>
2018-08-24 00:23:04 +0700

    fix timeline crash on no sections

--- src/screens/Timeline.js
+++ src/screens/Timeline.js
@@ -75,64 +75,68 @@ class Timeline extends React.Component {
   render() {
     const { id, details, refreshing } = this.props
 
-    let data = details.map((item) => ({
-      time: item.target_date ? moment(item.target_date).format('DD/MM/YYYY') : 'n/a',
-      title: item.subject,
-      description: item.description,
-      icon: item.isDone ? require('./../img/ic_done_white.png') : null,
-    }))
+    if (details.length > 0) {
+      let data = details.map((item) => ({
+        time: item.target_date ? moment(item.target_date).format('DD/MM/YYYY') : 'n/a',
+        title: item.subject,
+        description: item.description,
+        icon: item.isDone ? require('./../img/ic_done_white.png') : null,
+      }))
 
-    data.push({
-      id: 99,
-      time: moment(details[details.length - 1].target_date).format('DD/MM/YYYY'),
-      title: 'Project Finish',
-      percentage: 80, // TODO: implement percentage from server
-    })
+      data.push({
+        id: 99,
+        time: moment(details[details.length - 1].target_date).format('DD/MM/YYYY'),
+        title: 'Project Finish',
+        percentage: 80, // TODO: implement percentage from server
+      })
 
-    data.unshift({
-      id: 0,
-      time: moment(details[0].target_date).format('DD/MM/YYYY'),
-      title: 'Project Start',
-      percentage: 0,
-    })
+      data.unshift({
+        id: 0,
+        time: moment(details[0].target_date).format('DD/MM/YYYY'),
+        title: 'Project Start',
+        percentage: 0,
+      })
+    }
 
     return (
       <StyleProvider style={theme}>
         <Container>
           <LoadingView isShown={refreshing} solid />
-          <TimelineComponent
-            style={styles.timeline}
-            data={data}
-            columnFormat={'two-column'}
-            separator={false}
-            options={{
-              style: {paddingTop: 5, paddingBottom: 5},
-            }}
-            onEventPress={(e) => {}}
-            lineColor='gray'
-            renderDetail={(rowData, sectionId, rowId) => {
-              const textAlign = {textAlign: rowId % 2 === 0 ? 'left' : 'right'}
-              return (
-                <View style={{marginTop: -12}}>
-                  <Text style={[styles.dateText, textAlign]}>{rowData.time}</Text>
-                  <Text style={[styles.titleText, textAlign]}>{rowData.title}</Text>
-                  <Text style={[styles.descriptionText, textAlign]}>{rowData.description}</Text>
-                </View>
-              )
-            }}
-            renderCircle={(rowData, sectionId, rowId) => {
-              if (rowData.id === 0 || rowData.id === 99) {
+          {details.length > 0 &&
+            <TimelineComponent
+              style={styles.timeline}
+              data={data}
+              columnFormat={'two-column'}
+              separator={false}
+              options={{
+                style: {paddingTop: 5, paddingBottom: 5},
+              }}
+              onEventPress={(e) => {}}
+              lineColor='gray'
+              renderDetail={(rowData, sectionId, rowId) => {
+                const textAlign = {textAlign: rowId % 2 === 0 ? 'left' : 'right'}
                 return (
-                  <View style={styles.largeDot}>
-                    <Text style={{color: color.darkText, fontWeight: 'bold'}}>{rowData.percentage}%</Text>
+                  <View style={{marginTop: -12}}>
+                    <Text style={[styles.dateText, textAlign]}>{rowData.time}</Text>
+                    <Text style={[styles.titleText, textAlign]}>{rowData.title}</Text>
+                    <Text style={[styles.descriptionText, textAlign]}>{rowData.description}</Text>
                   </View>
                 )
-              }
-              return (
-                <View style={[styles.dot, {backgroundColor: colors[Math.floor(Math.random() * colors.length)]}]}/>
-              )
-            }}
-          />
+              }}
+              renderCircle={(rowData, sectionId, rowId) => {
+                if (rowData.id === 0 || rowData.id === 99) {
+                  return (
+                    <View style={styles.largeDot}>
+                      <Text style={{color: color.darkText, fontWeight: 'bold'}}>{rowData.percentage}%</Text>
+                    </View>
+                  )
+                }
+                return (
+                  <View style={[styles.dot, {backgroundColor: colors[Math.floor(Math.random() * colors.length)]}]}/>
+                )
+              }}
+            />
+          }
         </Container>
       </StyleProvider>
     )
